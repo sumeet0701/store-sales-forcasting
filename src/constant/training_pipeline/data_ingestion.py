8a73c5ad03aab4e0848a4cfb020a7747aa327cc9
sumeet <maheshwarisumeet128@gmail.com>
2023-06-09 08:03:18 +0530

    Contant folder is created .

---
+++ src/constant/training_pipeline/data_ingestion.py
@@ -0,0 +1,13 @@
+# Data Ingestion related variable
+DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
+DATA_INGESTION_ARTIFACT_DIR = "data_ingestion"
+DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir"
+DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
+DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
+DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+CONFIG_FILE_KEY = "config"
+
+
+FILE_PATH="file_path"
+FILE_NAME="file_name"
+DESTINATION_FOLDER= r"C:\Users\Sumeet Maheshwari\Desktop\end to end project\Store Sales Forcasting using Time series\store-sales-forcasting\cleaned_data"
---
+++ src/constant/training_pipeline/db_operation.py
@@ -0,0 +1,6 @@
+DATABASE_CLIENT_URL_KEY = "mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true"
+DATABASE_NAME_KEY = "Store_Sales_db"
+DATABASE_COLLECTION_NAME_KEY = "Store_Sales"
+DATABASE_TRAINING_COLLECTION_NAME_KEY = "Training"
+DATABASE_TEST_COLLECTION_NAME_KEY = "Test"
+
